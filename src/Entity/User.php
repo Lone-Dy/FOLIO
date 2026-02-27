55b7922fa6df0c15773ffd48f2038bb8e9671af3
Lone-Dy <dylangbelle@outlook.fr>
2026-02-27 16:54:44 +0100

    Mise à jour Entity/Repository

---
+++ src/Entity/User.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Entity;
+
+class User
+{
+    private ?int $id_user = null;
+    private string $nom;
+    private string $prenom;
+    private string $email;
+    private string $password;
+    private string $statut_compte;
+    private string $role;
+
+    public function getIdUser(): ?int
+    {
+        return $this->id_user;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->getEmail = $email;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getStatutCompte(): string
+    {
+        return $this->statut_compte;
+    }
+
+    public function setStatutCompte(string $statut_compte): self
+    {
+        $this->statut_compte = $statut_compte;
+        return $this;
+    }
+
+    public function getRole(): string
+    {
+        return $this->role;
+    }
+
+    public function setRole(string $role): self
+    {
+        $this->role = $role;
+        return $this;
+    }
+}
+?>
---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use \PDO;
+
+class UserRepository
+{
+    private PDO $pdo;
+
+    public function __construct(PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
+    // CRUD - CREATE
+
+    public function create(User $user): bool
+    {
+        $sql = "INSERT INTO user (nom, prenom, email, password, statut_compte, role) 
+    VALUES (:nom, :prenom, :email, :password, :statut_compte, :role)";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(":nom", $user->getNom());
+        $stmt->bindValue(":prenom", $user->getPrenom());
+        $stmt->bindValue(":email", $user->getEmail());
+        $stmt->bindValue(":password", $user->getPassword());
+        $stmt->bindValue(":statut_compte", $user->getStatutCompte() ?? 'actif');
+        $stmt->bindValue(":role", $user->getRole() ?? 'user');
+        $result = $stmt->execute();
+        return $result;
+    }
+
+    // CRUD - READ
+
+    public function findById(int $id): ?User
+    {
+        $stmt = $this->pdo->prepare("SELECT * FROM user WHERE id_user = :id");
+        $stmt->bindValue(":id", $id);
+        $stmt->execute();
+        $user = $stmt->fetch();
+        return $user ?: null;
+    }
+
+    public function findAll(): array
+    {
+        $stmt = $this->pdo->query("SELECT * from user");
+        $user = [];
+        while ($row = $stmt->fetch()) {
+            $user[] = new User();
+            $user->setNom($row['nom'])
+                ->setPrenom($row['prenom'])
+                ->setEmail($row['email'])
+                ->setRole($row['role'])
+                ->setStatutCompte($row['statut_compte']);
+
+            $users[] = $user;
+        }
+        return $user;
+    }
+
+    // CRUD - UPDATE
+
+public function update(User $user): bool {
+        $sql = "UPDATE user SET 
+                nom = :nom, 
+                prenom = :prenom, 
+                email = :email, 
+                statut_compte = :statut, 
+                role = :role 
+                WHERE id_user = :id";
+        
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute([
+            'nom'    => $user->getNom(),
+            'prenom' => $user->getPrenom(),
+            'email'  => $user->getEmail(),
+            'statut' => $user->getStatutCompte(),
+            'role'   => $user->getRole(),
+            'id'     => $user->getIdUser()
+        ]);
+    }
+
+    // CRUD - DELETE
+
+public function delete(int $id): bool {
+        $stmt = $this->pdo->prepare("DELETE FROM user WHERE id_user = ?");
+        return $stmt->execute([$id]);
+    }
+}
+
+?>
